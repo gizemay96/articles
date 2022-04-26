2d37c0d2bdb5c516eeca717a51c957eec6f87707
Gizem Ay <gizem.ay@profe.com.tr>
2022-04-26 14:24:31 +0300

    date format with user locale

--- src/app/modules/shared.module.ts
+++ src/app/modules/shared.module.ts
@@ -2,17 +2,24 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CardComponent } from '../components/card/card.component';
 import { RouterModule } from '@angular/router';
+import { MomentModule } from 'ngx-moment';
 
 @NgModule({
      imports: [
           CommonModule,
-          RouterModule
+          RouterModule,
+          MomentModule.forRoot({
+               relativeTimeThresholdOptions: {
+                 'm': 59
+               }
+             })
      ],
      declarations: [
           CardComponent
      ],
      exports: [
-          CardComponent
+          CardComponent,
+          MomentModule
      ]
 })
 export class SharedModule {}
